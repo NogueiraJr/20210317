aa395f655c66a38a9d81c31145a4aa5c1bc79b30
Nogueira <anogueirajr@gmail.com>
2021-03-17 20:59:43 -0300

    Segundo Commit

--- pythonHello.py
+++ pythonHello.py
@@ -6,3 +6,9 @@ num2 = 7.8
 soma = num1 + num2
 print('A soma de {0} e {1} eh {2}'.format(num1, num2, soma))
 
+my_str = "Olah meus amigos esta eh uma frase que serah ordenada"
+words = [word.lower() for word in my_str.split()]
+words.sort()
+print("Em ordem:")
+for word in words:
+   print(word)
