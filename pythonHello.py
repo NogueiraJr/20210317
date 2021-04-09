622b4343adce3e2a521d37b452868997f2e2fc90
Nogueira <anogueirajr@gmail.com>
2021-04-09 10:06:24 -0300

    Convert temperature in celsius to fahrenheit

--- pythonHello.py
+++ pythonHello.py
@@ -50,3 +50,14 @@ s = (a + b + c) / 2
 # calculate the area
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 print('The area of the triangle is %0.2f' %area)
+
+#
+
+# Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
