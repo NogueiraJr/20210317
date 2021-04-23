f6f484fd0022e34551c87dea8730c8a2637887db
Nogueira Jr <nogueirajr@hotmail.com>
2021-04-23 10:49:58 -0300

    Update pythonHello.py

--- pythonHello.py
+++ pythonHello.py
@@ -105,3 +105,10 @@ sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
 print('The solution are {0} and {1}'.format(sol1,sol2))
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\n")
